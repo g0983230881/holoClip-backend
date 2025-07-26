9a6621bd89f947d865f43838dd55f1b3fbb64c0b
Jordan <g0983230881@gmail.com>
2025-07-27 03:36:28 +0800

    add SpringBootServletInitializer to run main func.

--- src/main/java/com/HoloClip/Collector/CollectorApplication.java
+++ src/main/java/com/HoloClip/Collector/CollectorApplication.java
@@ -3,10 +3,17 @@ package com.HoloClip.Collector;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.HoloClip.Collector.mapper")
-public class CollectorApplication {
+public class CollectorApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CollectorApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CollectorApplication.class, args);
