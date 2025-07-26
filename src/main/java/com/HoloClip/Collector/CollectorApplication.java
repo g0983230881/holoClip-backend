e1941a0b3fe4bf5808fb7def2f3a76388f952a6b
Jordan <g0983230881@gmail.com>
2025-07-27 03:53:05 +0800

    test jar

--- src/main/java/com/HoloClip/Collector/CollectorApplication.java
+++ src/main/java/com/HoloClip/Collector/CollectorApplication.java
@@ -3,17 +3,10 @@ package com.HoloClip.Collector;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.HoloClip.Collector.mapper")
-public class CollectorApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(CollectorApplication.class);
-    }
+public class CollectorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CollectorApplication.class, args);
